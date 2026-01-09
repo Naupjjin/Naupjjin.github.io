bf7acb6ead8765c0829e77061305ccff717f5e76
Naupjjin <naup96721@gmail.com>
2026-01-09 23:53:00 +0800

    Site updated: 2026-01-09 23:52:44

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1767186126468";const preCache = ["/images/taichi.png","https://i.postimg.cc/bJwcgqPx/131939316-p0.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1767973961147";const preCache = ["/images/taichi.png","https://i.postimg.cc/bJwcgqPx/131939316-p0.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
